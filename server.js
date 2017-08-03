9b947990c96d3edb1a0e4dd3a3d70542cc2b6101
Philip Curley <curleypg@gmail.com>
2017-08-03 00:03:03 -0400

    Add caching to reduce requests to Github API

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const express = require('express'),
  path = require('path'),
  app = express();
  request = require('request');
+ Cache = require('streaming-cache');
+
+localCache = new Cache({
+ maxAge: 5000 // max age in ms of items stored in memcache
+});
 
 app.use(express.static(path.join(__dirname, '/public')));
 
@@ -11,16 +16,22 @@ app.get('/', (req, res) => {
 });
 
 app.get('/users/:userId/repos', (req, res) => {
-  request({
-    url: 'https://api.github.com/users/' + req.params.userId + '/repos?type=owner',
-    headers: {
-      'User-Agent': req.params.userId
-    }
-  })
-  .on('error', function(err){
-    console.log(err);
-  })
-  .pipe(res);
+  let cached = localCache.get('test');
+  if (cached) {
+    cached.pipe(res);
+  } else {
+    request({
+      url: 'https://api.github.com/users/' + req.params.userId + '/repos?type=owner',
+      headers: {
+        'User-Agent': req.params.userId
+      }
+    })
+    .on('error', function(err){
+      console.log(err);
+    })
+    .pipe(localCache.set('test'))
+    .pipe(res);
+  }
 });
 
 app.listen(config.port, config.host, () => console.log(`App running on localhost:${config.port}`));
